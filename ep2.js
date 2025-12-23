f3822e1c683e657d0e8b35db4e550cd89be718c4
ahma662 <ahmii91234@gmail.com>
2025-12-23 17:17:00 +0500

    fix:Bugs & remove uncertainities

--- ep2.js
+++ ep2.js
@@ -1,25 +1,3 @@
-// document.getElementById('n-1').onclick = function () {
-//     alert("Clicked Img");
-// }
-// document.getElementById('images').addEventListener('click', function (e) {
-//     console.log("External Clicked");
-// }, false)  //Bubbling ---Inner First_Outer Last---
-document.getElementById('images').addEventListener('click', function (e) {
-    console.log("External Clicked");
-}, false);  //Event Capturing --outer first_Inner Last--- 
-
-document.getElementById('n-2').addEventListener('click', function () {
-    console.log("Img2 is Clicked");
-    e.stopPropagation();
-}, false);
-
-document.getElementById('google').addEventListener('click', function (e) {
-    console.log("Google Clicked");
-    e.preventDefault();
-}, false);
-
-
-
 let count = 0;
 const countdisplay = document.getElementById('count');
 const increasebtn = document.getElementById('increase');
